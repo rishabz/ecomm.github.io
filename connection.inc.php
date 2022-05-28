cde60c7524ff40cc0d1c1b878ad8111ae09df09b
Rishabz <41898461+rishabz@users.noreply.github.com>
2022-05-28 18:09:19 +0530

    ecomm

---
+++ connection.inc.php
@@ -0,0 +1,9 @@
+<?php
+session_start();
+$con=mysqli_connect("localhost","root","","ecomm");
+define('SERVER_PATH',$_SERVER['DOCUMENT_ROOT'].'ecomm/');
+define('SITE_PATH','http://127.0.0.1/ecomm/');
+
+define('PRODUCT_IMAGE_SERVER_PATH',SERVER_PATH.'media/product/');
+define('PRODUCT_IMAGE_SITE_PATH',SITE_PATH.'media/product/');
+?>
